501a97fbac649440d082bdf2e1d10c2bf5dec42b
weijun huang <luck938938@hotmail.com>
2018-11-13 19:50:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,14 +45,14 @@ function checkCollision(rock) {
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
                */
-    if ((rockLeftEdge < dodgerLeftEdge)
-        && (rockRightEdge > dodgerLeftEdge)) {
+    if ((rockLeftEdge <= dodgerLeftEdge)
+        && (rockRightEdge >= dodgerLeftEdge)) {
           return false;
-         } else if((rockLeftEdge > dodgerLeftEdge)
-                   && (rockRightEdge <dodgerRightEdge)){
+         } else if((rockLeftEdge >= dodgerLeftEdge)
+                   && (rockRightEdge <=dodgerRightEdge)){
               return false;
-          }else if((rockLeftEdge < dodgerRightEdge)
-                    && (rockRightEdge > dodgerRightEdge)){
+          }else if((rockLeftEdge <= dodgerRightEdge)
+                    && (rockRightEdge >= dodgerRightEdge)){
                       return false
                     }else{
                      return true
